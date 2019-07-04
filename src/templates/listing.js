2faebd5ed59f2d94d6043c7bbff4509b94be90b2
Chadwick Maycumber <ChadwickMaycumber@Chads-Mac.local>
2019-07-03 20:52:19 -0700

    Added in multiple images on pages

--- src/templates/listing.js
+++ src/templates/listing.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 import config from '../utils/siteConfig'
@@ -10,12 +10,46 @@ import PostDate from '../components/PostDate'
 import SEO from '../components/SEO'
 import { Heading, Text } from 'grommet'
 import styled from 'styled-components'
+import SwipeableViews from 'react-swipeable-views'
+
+const InfoWrapper = styled.div`
+  display: flex;
+  margin: 0 auto 2em;
+  max-width: ${props => props.theme.sizes.maxWidthCentered};
+  justify-content: flex-start;
+  align-items: center;
+`
+
+const Dots = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 1em 0;
+`
+
+const Dot = styled.div`
+  padding: 5px;
+  border-radius: 10px;
+  cursor: pointer;
+  margin: 0 4px;
+  border: 1px solid black;
+  background: ${props => (props.selected ? 'black' : 'transparent')};
+`
+
+const InfoItems = styled(Heading)``
+
+const InfoValues = styled(Text)`
+  font-weight: bold;
+  padding: 0em 1em;
+`
 
 const ListingTemplate = ({ data }) => {
   const {
     title,
     heroImage,
     publishDate,
+    additionalImages,
     slug,
     beds,
     baths,
@@ -24,20 +58,7 @@ const ListingTemplate = ({ data }) => {
   } = data.contentfulListing
   const postNode = data.contentfulListing
 
-  const InfoWrapper = styled.div`
-    display: flex;
-    margin: 0 auto 2em;
-    max-width: ${props => props.theme.sizes.maxWidthCentered};
-    justify-content: flex-start;
-    align-items: center;
-  `
-
-  const InfoItems = styled(Heading)``
-
-  const InfoValues = styled(Text)`
-    font-weight: bold;
-    padding: 0em 1em;
-  `
+  const [selected, setSelected] = useState(0)
 
   return (
     <Layout>
@@ -45,7 +66,35 @@ const ListingTemplate = ({ data }) => {
         <title>{`${title} - ${config.siteTitle}`}</title>
       </Helmet>
       <SEO pagePath={slug} postNode={postNode} postSEO />
-      <Hero title={title} image={heroImage} height={'70vh'} />
+      {additionalImages ? (
+        <React.Fragment>
+          <SwipeableViews
+            onChangeIndex={(index, indexLatest, meta) => setSelected(index)}
+            index={selected}
+            enableMouseEvents
+          >
+            {additionalImages.map((image, index) => {
+              return (
+                <Hero title={title} image={image} key={index} height={'70vh'} />
+              )
+            })}
+          </SwipeableViews>
+          <Dots>
+            {additionalImages.length > 1 &&
+              additionalImages.map((image, index) => {
+                return (
+                  <Dot
+                    onClick={() => setSelected(index)}
+                    selected={index === selected}
+                    key={index}
+                  />
+                )
+              })}
+          </Dots>
+        </React.Fragment>
+      ) : (
+        <Hero title={title} image={heroImage} height={'70vh'} />
+      )}
       <Container>
         <InfoWrapper>
           <InfoItems level={3}>Beds: </InfoItems>
@@ -74,6 +123,17 @@ export const query = graphql`
     contentfulListing(slug: { eq: $slug }) {
       title
       heroImage {
+        title
+        fluid(maxWidth: 1800) {
+          ...GatsbyContentfulFluid_withWebp_noBase64
+        }
+        ogimg: resize(width: 1800) {
+          src
+          width
+          height
+        }
+      }
+      additionalImages {
         title
         fluid(maxWidth: 1800) {
           ...GatsbyContentfulFluid_withWebp_noBase64
